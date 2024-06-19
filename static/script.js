30146927afd0631a0334cd7c1860f8a4dfb4f5e1
rabboni <ocholigocholi@gmail.com>
2024-06-19 02:34:13 +0100

    Added Exam mode

--- static/script.js
+++ static/script.js
@@ -6,17 +6,25 @@ document.addEventListener("DOMContentLoaded", function() {
     const pillBottle = document.querySelector(".pill-bottle");
     const pill = document.querySelector("#pill");
     const submitButton = document.querySelector("#toggle");
+    const examButton = document.querySelector("#exam_mode");
+    let exam_mode = "Off";
 
     let selectedAnswer = null;
 
-    questionDisplay.addEventListener("click", (e) => {
-        if (e.target.tagName === "INPUT") {
-            const answer = e.target.nextElementSibling.textContent;
-            selectedAnswer = answer;
-            document.getElementById("next_answer").value = selectedAnswer
-            document.getElementById("end_answer").value = selectedAnswer
-            fillPillBottle();
+    
+    examButton.addEventListener("click", () => {
+        if (examButton.innerHTML === "Off") {
+            examButton.innerHTML = "On";
+            exam_mode = "On";
+            examButton.style.backgroundColor = "red";
+        } else {
+            examButton.innerHTML = "Off";
+            exam_mode = "Off";
+            examButton.style.backgroundColor = "#4c77af";
         }
+        console.log(`Exam Mode: ${exam_mode}`);
+        document.getElementById("mode_choice1").value = exam_mode;
+        document.getElementById("mode_choice2").value = exam_mode;
     });
 
     submitButton.addEventListener("click", () => {
@@ -33,6 +41,16 @@ document.addEventListener("DOMContentLoaded", function() {
         }
         console.log(`Answer submitted: ${selectedAnswer}`);
         
+    });
+    
+    questionDisplay.addEventListener("click", (e) => {
+        if (e.target.tagName === "INPUT") {
+            const answer = e.target.nextElementSibling.textContent;
+            selectedAnswer = answer;
+            document.getElementById("next_answer").value = selectedAnswer
+            document.getElementById("end_answer").value = selectedAnswer
+            fillPillBottle();
+        }
     });
     
 
