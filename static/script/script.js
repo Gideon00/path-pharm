a531f9696f7c9374b03b189e176c248b17ea8a03
rabboni <ocholigocholi@gmail.com>
2024-10-23 23:24:24 +0100

    Update script.js

--- static/script/script.js
+++ static/script/script.js
@@ -1,12 +1,12 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    const examButton = document.querySelector("#exam_mode");
+    const examButton = document.querySelector(".myBtn");
     let examMode = "Off";
 
     examButton.addEventListener("click", () => {
         examMode = examMode === "Off" ? "On" : "Off";
         examButton.innerHTML = examMode;
-        examButton.style.backgroundColor = examMode === "On" ? "red" : "#4c77af";
+        examButton.style.backgroundColor = examMode === "On" ? "rgb(151, 87, 2)" : "rgb(245, 204, 150)";
         console.log(`Exam Mode: ${examMode}`);
         document.getElementById("mode_choice1").value = examMode;
         document.getElementById("mode_choice2").value = examMode;
